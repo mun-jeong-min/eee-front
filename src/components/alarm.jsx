2c58ea8faeb7defe957b8678b4141459f0d8218c
문정민 <moondeve41@gmail.com>
2023-04-28 14:12:26 +0900

    ADD :: auth login service

--- src/components/alarm.jsx
+++ src/components/alarm.jsx
@@ -1,15 +1,14 @@
-import star from './static/star.png'
+import star from "./static/star.png";
 
 const Alarm = () => {
+  return (
+    <div className='App'>
+      <div className='alarm'>
+        <img className='alarmImg' src={star} alt='별' />
+        <p className='alarmP'>cats 운동이 새로 추가 되었습니다.</p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="App">
-            <div className="alarm">
-                <img className='alarmImg' src={star} alt="별" />
-                <p className='alarmP'>cats 운동이 새로 추가 되었습니다.</p>
-            </div>
-        </div>
-    )
-}
-
-export default Alarm
+export default Alarm;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,26 +1,70 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import logo from './static/logo.png'
+import { Link } from "react-router-dom";
+import logo from "./static/logo.png";
+import { instance } from "../util/instance";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [data, setData] = useState("");
+  const [code, setCode] = useState("");
 
-    return (
-        <div className='App'>
-            <div>
-                <img src={logo} alt="메인로고" width={300} height={200} />
-                <p className='logoP'>동기부여 운동 서비스</p>
-            </div>
-            <div>
-                <p className='emailP'>E-MAIL</p>
-                <input type="text" placeholder='이메일을 입력하세요' className='mainInput'></input>
-                <button className='mainButton'>입력</button>
-            </div>
-            <div className='인증코드'>
-                <input type="text" placeholder='인증코드 입력' className='mainInput'></input>
-                <Link to="/home"><button className='mainButton'>시작</button></Link>
-            </div>
-        </div>
-    )
-}
+  const authCode = (e) => {
+    setCode(e.target.value);
+  };
 
-export default Main
+  const saveUserEmail = (e) => {
+    setData(e.target.value);
+  };
+
+  const verifyEmail = () => {
+    instance.post("/user", {
+      email: data,
+    });
+  };
+
+  const loginAuthCode = () => {
+    instance
+      .post("/user/signup", {
+        code: code,
+        email: data,
+      })
+      .then(function (res) {
+        console.log(res);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <div>
+        <img src={logo} alt='메인로고' width={300} height={200} />
+        <p className='logoP'>동기부여 운동 서비스</p>
+      </div>
+      <div>
+        <p className='emailP'>E-MAIL</p>
+        <input
+          type='text'
+          placeholder='이메일을 입력하세요'
+          className='mainInput'
+          value={data}
+          onChange={saveUserEmail}></input>
+        <button className='mainButton' onClick={verifyEmail}>
+          입력
+        </button>
+      </div>
+      <div className='인증코드'>
+        <input
+          type='text'
+          placeholder='인증코드 입력'
+          className='mainInput'
+          value={code.code}
+          onChange={authCode}></input>
+        <Link to='/home'>
+          <button className='mainButton' onClick={loginAuthCode}>
+            시작
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
