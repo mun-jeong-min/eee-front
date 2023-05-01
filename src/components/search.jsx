7a6170958aec2d05a4956693f33f90200ae7929f
문정민 <moondeve41@gmail.com>
2023-05-01 14:49:27 +0900

    EDIT :: category 검색 후 값 반환

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
+import { instance } from "../util/instance";
+
 const Search = () => {
+  const [data, setData] = useState("");
+  const [list, setList] = useState([]);
+  const input = (e) => {
+    setData(e.target.value);
+  };
+
+  const search = () => {
+    instance
+      .post("/practice/read", {
+        category: data,
+      })
+      .then(function (res) {
+        setList(res.data.list);
+      });
+  };
+
   return (
     <div className='App'>
       <div>
-        <p className='searchP'>검색</p>
         <p>카테고리를 입력하세요</p>
         <div className='search'>
-          <input className='searchInput' type='text' />
-          <button>X</button>
-          <button>v</button>
+          <input
+            className='searchInput'
+            type='text'
+            value={data}
+            onChange={input}
+          />
+          <button onClick={search}>검색</button>
         </div>
+        {list.map((v, i) => {
+          console.log(v);
+          return <div className='categoryInput'>{v.content}</div>;
+        })}
       </div>
     </div>
   );
