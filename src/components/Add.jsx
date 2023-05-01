bc7518ddcd223f00da2b5c7020d2159b5b0f7055
문정민 <moondeve41@gmail.com>
2023-05-01 15:40:11 +0900

    EDIT :: 운동 및 행동 추가 로직

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,93 @@
+import { useState } from "react";
+import { instance } from "../util/instance";
+
 const Add = () => {
+  const [ex, setEx] = useState("");
+  const [pr, setPr] = useState("");
+  const [category, setCategory] = useState("");
+
+  const changeEx = (e) => {
+    setEx(e.target.value);
+  };
+
+  const changePr = (e) => {
+    setPr(e.target.value);
+  };
+
+  const changeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const clickEx = () => {
+    instance
+      .post("/practice/category", {
+        category: ex,
+      })
+      .then(function (res) {
+        if (res.status == 200) {
+          window.alert("전송 성공!");
+          setEx("");
+        } else {
+          window.alert("전송 실패!");
+        }
+      });
+  };
+
+  const clickPr = () => {
+    instance
+      .post("/practice", {
+        content: pr,
+        category: category,
+      })
+      .then(function (res) {
+        if (res.status == 200) {
+          window.alert("전송 성공!");
+          setPr("");
+          setCategory("");
+        } else {
+          window.alert("전솔 실패!");
+        }
+      });
+  };
+
   return (
     <div className='App'>
-      <div>운동 추가</div>
-      <div>행동 추가</div>
+      <div className='add'>
+        <div className='addEx'>
+          <p>운동 추가</p>
+          <div className='addDiv'>
+            <input
+              type='text'
+              placeholder='Enter text'
+              className='addInput'
+              value={ex}
+              onChange={changeEx}
+            />
+            <button className='addButton' onClick={clickEx}>
+              ADD
+            </button>
+          </div>
+        </div>
+        <div className='addPr'>
+          <p>행동 추가</p>
+          <input
+            type='text'
+            placeholder='Enter text'
+            className='addInput'
+            value={pr}
+            onChange={changePr}
+          />
+          <input
+            type='text'
+            placeholder='운동 카테고리 입력'
+            value={category}
+            onChange={changeCategory}
+          />
+          <button className='addButton' onClick={clickPr}>
+            ADD
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
