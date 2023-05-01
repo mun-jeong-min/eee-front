fa24e943388bf93e6e093b98493c2aa22606ed58
문정민 <moondeve41@gmail.com>
2023-05-01 15:17:21 +0900

    ADD :: question service

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import { instance } from "../util/instance";
+
 const Question = () => {
+  const [data, setData] = useState("");
+  const [list, setList] = useState([]);
+
+  const question = (e) => {
+    setData(e.target.value);
+  };
+
+  useEffect(() => {
+    instance.get("/question").then(function (res) {
+      setList(res.data.list);
+    });
+  }, []);
+
+  const clickButton = () => {
+    instance
+      .post("/question", {
+        content: data,
+      })
+      .then(function (res) {
+        if (res.status == 200) {
+          window.alert("요청 성공!");
+          setData("");
+        } else {
+          window.alert("요청 실패!");
+        }
+      });
+  };
+
   return (
     <div className='App'>
       <div>문의</div>
+      <div className='questionDiv'>
+        <input
+          type='text'
+          placeholder='ENTER'
+          className='questionInput'
+          value={data}
+          onChange={question}
+        />
+        <button className='questionButton' onClick={clickButton}>
+          BUTTON
+        </button>
+      </div>
+      <div className='questionResult'>
+        {list.map((v, i) => {
+          return <div className='questionCnt'>{v.content}</div>;
+        })}
+      </div>
     </div>
   );
 };
